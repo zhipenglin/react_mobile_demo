c35672e1f4b1c38a46d8e0016e5b2624f74d7d10
linzhipeng <zhipeng.lin@ifchange.com>
2017-07-25 12:00:38 +0800

    修改了环境变量

--- src/store.js
+++ src/store.js
@@ -2,12 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
-import createHistory from 'history/createHashHistory';
+import createHistory from 'history/createBrowserHistory';
+import createHashHistory from 'history/createHashHistory';
 import rootReducer from './reducers/rootReducer';
 
 const loggerMiddleware = createLogger();
 
-export const history = createHistory();
+export const history =
+  process.env.REACT_APP_TARGET === 'github'
+    ? createHashHistory()
+    : createHistory();
 
 function configureStore(preloadedState) {
   return createStore(
